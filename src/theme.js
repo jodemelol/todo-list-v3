99fdd23250ae2a7b275be91071f55fc24035f07a
jodemelol <jchivata97@gmail.com>
2022-06-08 18:47:22 -0500

    Add background

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,8 @@ export default extendTheme({
     styles:{
         global:{
             'html,body':{
-                fontFamily:"Caveat, cursive"
+                fontFamily:"Caveat, cursive",
+                bg:"radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)"
             },
         },
     },
